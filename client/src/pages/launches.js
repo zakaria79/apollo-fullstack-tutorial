413b46801c43a1b73db4e12df8e408dfc9ef79d6
Zakaria OTHMANE <othmane.zakaria79@gmail.com>
2019-10-04 20:54:33 +0200

    6. Fetch data with queries

--- client/src/pages/launches.js
+++ client/src/pages/launches.js
@@ -1,5 +1,83 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { LaunchTitle, Header, Button, Loading } from '../components';
+
+const GET_LAUNCHES = gql`
+	query launchList($after: String) {
+		launches(after: $after) {
+			cursor
+			hasMore
+			launches {
+				id
+				isBooked
+				rocket {
+					id
+					name
+				}
+				mission {
+					name
+					missionPatch
+				}
+			}
+		}
+	}
+`;
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
 
 export default function Launches() {
-  return <div />;
+	const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
+	if (loading) {
+		return <Loading />;
+	}
+	if (error) {
+		return <p>ERROR</p>;
+	}
+
+	return (
+		<Fragment>
+			<Header />
+			{data.launches &&
+				data.launches.launches &&
+				data.launches.launches.map((launch) => <LaunchTitle key={launch.id} launch={launch} />)}
+			{data.launches &&
+			data.launches.hasMore && (
+				<Button
+					onClick={() =>
+						fetchMore({
+							variables: {
+								after: data.launches.cursor
+							},
+							updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+								if (!fetchMoreResult) return prev;
+								return {
+									...fetchMoreResult,
+									launches: {
+										...fetchMoreResult.launches,
+										launches: [ ...prev.launches.launches, ...fetchMoreResult.launches.launches ]
+									}
+								};
+							}
+						})}
+				>
+					Load More
+				</Button>
+			)}
+		</Fragment>
+	);
 }
